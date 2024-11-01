55916045ca051911938ebe2f07464fefa8f71f9f
Charleston Campos <charlinhocjc@gmail.com>
2024-11-01 18:52:15 -0300

    feat: adding timer feature

--- src/app/shared/components/timer-control/timer-control.component.ts
+++ src/app/shared/components/timer-control/timer-control.component.ts
@@ -1,12 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-timer-control',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './timer-control.component.html',
   styleUrl: './timer-control.component.scss'
 })
 export class TimerControlComponent {
 
+  timerFormat = '';
+
+  isTimerStarted = false;
+
+  timerInSeconds = 30;
+
+  private intervalId: any;
+
+  constructor() {
+    this.configTimer();
+  }
+
+  onStartClick(): void {
+    this.intervalId = setInterval(() => {
+      this.countdown()
+    }, 1000);
+
+    this.isTimerStarted = true;
+  }
+
+  onPauseClick(): void {
+    this.isTimerStarted = false;
+    clearInterval(this.intervalId);
+  }
+
+  private countdown(): void {
+    this.timerInSeconds -= 1;
+    this.configTimer();
+  }
+
+  private configTimer(): void {
+    this.timerFormat = new Date(this.timerInSeconds * 1000)
+      .toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'});
+  }
 }

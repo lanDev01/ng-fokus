c9d8e713af15a08d6ed30937cdcc2f0216964eec
Charleston Campos <charlinhocjc@gmail.com>
2024-11-01 18:05:26 -0300

    feat: HTML & CSS structuct finished

---
+++ src/app/shared/components/timer-control/timer-control.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timer-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './timer-control.component.html',
+  styleUrl: './timer-control.component.scss'
+})
+export class TimerControlComponent {
+
+}
